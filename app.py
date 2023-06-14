964b5ec6274fe548bc4dac3eec4d546c2fa26c27
Akash <akumar415129.ak@gmail.com>
2023-06-14 19:11:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,51 +11,37 @@ creds = ServiceAccountCredentials.from_json_keyfile_name("sheet.json", scope)
 client = gspread.authorize(creds)
 
 
+
 @app.get('/')
 def upload():
     return render_template('upload-excel.html')
 
-
 @app.post('/view')
 def view():
-    sheet = client.open("Test Sheet").get_worksheet_by_id(116082223)
+    sheet = client.open("Live Quiz Automations").get_worksheet_by_id(116082223)
     sheetData = sheet.get_all_records()
-    # Read the File using Flask request
     file = request.files['file']
-    # save file in local directory
     df = pd.DataFrame(sheetData)
-    # print("Sheet Data : ", df)
-    # Parse the data as a Pandas DataFrame type
-    sessionName = pd.read_excel(
-        file, sheet_name='Attendance', header=4, usecols=[1], nrows=1)
-    data = pd.read_excel(file, sheet_name='Attendance',
-                         header=10, usecols=[0, 1, 2, 3])
+    sessionName = pd.read_excel(file, sheet_name='Attendance', header=4, usecols=[1], nrows=1)
+    data = pd.read_excel(file, sheet_name='Attendance', header=10, usecols=[0, 1, 2, 3])
     newData = data[~(data['Email'].isnull())]
     newData['First Joined'] = newData['First Joined'].str[:-13]
-    pollingData = pd.read_excel(
-        file, sheet_name='Polling Results', header=8, usecols=[0, 1, 2])
+    pollingData = pd.read_excel(file, sheet_name='Polling Results', header=8, usecols=[0, 1, 2])
     newPollingData = pollingData[
         pollingData[['First Name', 'Last Name']].apply(
-            lambda row: (row['First Name'], row['Last Name']
-                         ) in newData[['First Name', 'Last Name']].values,
+            lambda row: (row['First Name'], row['Last Name']) in newData[['First Name', 'Last Name']].values,
             axis=1
         )
     ]
-    newPollingData['Total answers correct'] = newPollingData['Total answers correct'].fillna(
-        0)
-
-    totalPolledData = pd.read_excel(
-        file, sheet_name='Polling Results', header=3, usecols='D:BA', nrows=0)
+    newPollingData['Total answers correct'] = newPollingData['Total answers correct'].fillna(0)
+    totalPolledData = pd.read_excel(file, sheet_name='Polling Results', header=3, usecols='D:V', nrows=0)
     totalPolledColumns = totalPolledData.columns.tolist()
-    polledCount = sum(
-        1 for column in totalPolledColumns if 'Not Polled' not in column)
+    polledCount = sum(1 for column in totalPolledColumns if 'Not Polled' not in column)
 
-    totalAttempted = pd.read_excel(
-        file, sheet_name='Polling Results', header=8, usecols="A:BA")
+    totalAttempted = pd.read_excel(file, sheet_name='Polling Results', header=8, usecols="A:V")
     newTotalAttempted = totalAttempted[
         totalAttempted[['First Name', 'Last Name']].apply(
-            lambda row: (row['First Name'], row['Last Name']
-                         ) in newData[['First Name', 'Last Name']].values,
+            lambda row: (row['First Name'], row['Last Name']) in newData[['First Name', 'Last Name']].values,
             axis=1
         )
     ]
@@ -72,17 +58,17 @@ def view():
                 continue
             else:
                 attemptCount += 1
-        finalData.loc[index,
-                      'Total Attempted'] = attemptCount if attemptCount > 0 else 0
+        finalData.loc[index, 'Total Attempted'] = attemptCount if attemptCount > 0 else 0
         index += 1
-    filteredFinalData = finalData.drop_duplicates(
-        subset=['Email', 'Session ID'])
-    uniqueRows = filteredFinalData[~filteredFinalData.isin(
-        df.to_dict(orient='list')).all(axis=1)]
-
+    # df = df._append(finalData, ignore_index=True
+    filteredFinalData = finalData.drop_duplicates(subset=['Email', 'Session ID'])
+    print(filteredFinalData)
+    uniqueRows = filteredFinalData[~filteredFinalData.isin(df.to_dict(orient='list')).all(axis=1)]
     data = uniqueRows.values.tolist()
-    print(data)
     if len(data) > 0:
         sheet.append_rows(data, value_input_option='USER_ENTERED')
 
     return uniqueRows.to_html()
+
+if __name__ == '__main__':
+    app.run(debug=True)
